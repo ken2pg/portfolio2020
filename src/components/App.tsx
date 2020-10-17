92d7e3bafbb32c44010295f5deeea8218882d22f
kento <jst34zais9102z@gmail.com>
2020-10-17 20:23:39 +0900

    各コンポーネントを追加

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import About from './About';
+import Top from './Top';
 
 const App = () => {
-  return <>aaa</>;
+  return (
+    <>
+      <Top />
+      <About />
+    </>
+  );
 };
 
 export default App;
