16325c624c80fa3adfbac89ff1ee164885392234
kento <jst34zais9102z@gmail.com>
2020-10-18 20:17:22 +0900

    css一部変更、メディアクエリの追加

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -2,24 +2,37 @@ import React, { useEffect, useState } from 'react';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 
+const Image = '/static/img1.png';
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     width: '100%',
-    height: '300px',
-    borderBottom: '1px solid gray',
+    height: '100%',
+    // borderBottom: '1px solid gray',
+    // backgroundImage: `url(${Image})`,
+    // backgroundSize: '100% 100%',
+    // ['@media(max-width:1024px)']: {
+    //   height: '300px',
+    // },
   },
   title: {
     display: 'flex',
+    margin: '190px 0',
     justifyContent: 'center',
-    marginTop: '190px',
+    // lineHeight: '500px',
     fontSize: '48px',
+    // color: 'White',
+    ['@media(max-width:1024px)']: {
+      width: '95%',
+      fontSize: '40px',
+      textAlign: 'center',
+    },
   },
 }));
 const Top = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Typography className={classes.title}>Kento's Portfolio Site.</Typography>
+      <Typography className={classes.title}>Kento's Portfolio Site</Typography>
     </div>
   );
 };
