105f82091ebe8d411bbf1e7e602404df9ef5d6e3
kento <jst34zais9102z@gmail.com>
2020-10-18 16:15:11 +0900

    新規作成コンポーネントを追加

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import About from './About';
+import Skills from './Skills';
 import Top from './Top';
+import Works from './Works';
 
 const App = () => {
   return (
     <>
       <Top />
       <About />
+      <Skills />
+      <Works />
     </>
   );
 };
